ef5c83aae675c508a4fd35626509568f924fe383
Thiebaud Modoux <thiebaud.modoux@epfl.ch>
2016-03-09 10:55:11 +0100

    Remove old app

--- oldapp/app/src/main/java/ch/epfl/lsi/ironICpp/Electrodes.java
+++
@@ -1,6 +0,0 @@
-package ch.epfl.lsi.ironICpp;
-
-/**
- * Created by Modoux Thiebaud on 18.02.2016.
- */
-public enum Electrodes {Glucose, Lactate, Bilirubin, Sodium, Potassium, PH, Temperature}
--- oldapp/app/src/main/java/ch/epfl/lsi/ironICpp/electrodeFactory/Measurement.java
+++
@@ -1,8 +0,0 @@
-package ch.epfl.lsi.ironICpp.electrodeFactory;
-
-/**
- * Created by Thieb on 19.02.2016.
- */
-public interface Measurement {
-    public void save();
-}
